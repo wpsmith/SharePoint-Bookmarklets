58c64a9fd929ad2a0c021c65601b74f26b6f1aae
Travis Smith <t@wpsmith.net>
2014-12-13 10:45:44 -0500

    Semicolon

--- src/SP.ServerVerionPatchLevel.js
+++ src/SP.ServerVerionPatchLevel.js
@@ -12,4 +12,4 @@
 
 (function (w) {
     w.location.href = ('undefined' !== typeof _spPageContextInfo ? _spPageContextInfo.webAbsoluteUrl : w.location.origin + L_Menu_BaseUrl) + "/_vti_pvt/Service.cnf";
-})(window)
+})(window);
