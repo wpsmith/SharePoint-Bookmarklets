12b88331b1e656f3719ced583079340da711ca28
Travis Smith <t@wpsmith.net>
2014-12-13 10:47:43 -0500

    New - Site Usage

---
+++ src/SP.SiteUsage.js
@@ -0,0 +1,15 @@
+/**
+ * Redirects Users to Site Usage
+ *
+ * Copyright (c) 2014, Travis Smith 2014
+ * @project SharePoint Bookmarks
+ * @file SP.SiteUsage.js
+ * @author Travis Smith <me@travislsmith.com>
+ * @link http://travislsmith.com/sharepoint-bookmarks/
+ * @date 12/13/2014.
+ * @version 0.0.1
+ */
+
+javascript:(function (w) {
+    w.location.href = _spPageContextInfo.webAbsoluteUrl + "/_layouts/usage.aspx";
+})(window)
